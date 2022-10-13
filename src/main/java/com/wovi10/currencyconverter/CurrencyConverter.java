fcbf792d798235d225608e14105d0562795f63d5
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-14 10:29:15 +1100

    Reformatted

--- src/main/java/com/wovi10/currencyconverter/CurrencyConverter.java
+++ src/main/java/com/wovi10/currencyconverter/CurrencyConverter.java
@@ -1,6 +1,5 @@
 package com.wovi10.currencyconverter;
 
-import com.wovi10.currencyconverter.utils.CurrencyConstants;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -48,34 +47,38 @@ public class CurrencyConverter extends Application {
     private Button createConvertButton() {
         Button convertButton = new Button();
         createDefaultButton(convertButton);
-        convertButton.setOnAction( actionEvent ->
+        convertButton.setOnAction(actionEvent ->
                 convertInput()
         );
         return convertButton;
     }
-    private void createDefaultButton(Button button){
+
+    private void createDefaultButton(Button button) {
         button.setLayoutX(STANDARD_INDENT + TEXTFIELD_WIDTH);
         button.setLayoutY(STANDARD_HEIGHT + 60);
         button.setText(CONVERT_TEXT);
     }
+
     private void convertInput() {
         setInput();
         calculateConversion();
         setOutput();
     }
+
     private void setInput() {
         input_int = Double.parseDouble(inputValuta.getText());
     }
+
     private void calculateConversion() {
         Currency currencyInput = defaultCurrency;
         for (Currency currency : Currencies) {
-            if (currency.getName().equals(valutaFrom_CB.getValue())){
+            if (currency.getName().equals(valutaFrom_CB.getValue())) {
                 currencyInput = currency;
             }
         }
         Currency currencyOutput = defaultCurrency;
         for (Currency currency : Currencies) {
-            if (currency.getName().equals(valutaTo_CB.getValue())){
+            if (currency.getName().equals(valutaTo_CB.getValue())) {
                 currencyOutput = currency;
             }
         }
@@ -83,22 +86,25 @@ public class CurrencyConverter extends Application {
         String nameCurrencyOutput = currencyOutput.getAbbreviation();
         output_int = convert(nameCurrencyInput, nameCurrencyOutput, input_int);
     }
+
     private Double convert(String nameCurrencyInput, String nameCurrencyOutput, Double amount) {
         double output;
         Double exchangeValue = getExchangeValue(nameCurrencyInput, nameCurrencyOutput);
         output = amount * exchangeValue;
         return output;
     }
-    private Double getExchangeValue(String abbreviationInput, String abbreviationOutput){
+
+    private Double getExchangeValue(String abbreviationInput, String abbreviationOutput) {
         Double output = DEFAULT_EXCHANGEVALUE;
         for (Currency currency : Currencies) {
-            if (abbreviationInput.equals(currency.getAbbreviation())){
+            if (abbreviationInput.equals(currency.getAbbreviation())) {
                 HashMap<String, Double> exchangeValues = currency.getExchangeValues();
                 output = exchangeValues.get(abbreviationOutput);
             }
         }
         return output;
     }
+
     private void setOutput() {
         String formatted_output = FORMAT.format(output_int);
         outputValuta.setText(formatted_output);
@@ -110,21 +116,25 @@ public class CurrencyConverter extends Application {
         ComboBox<Object> comboBox = createComboBox(STANDARD_HEIGHT);
         return fillComboBox(comboBox);
     }
+
     private ComboBox<Object> createSecondComboBox() {
         ComboBox<Object> comboBox = createComboBox(STANDARD_HEIGHT + 30);
         return fillComboBox(comboBox);
     }
+
     private ComboBox<Object> createComboBox(int layoutY) {
         ComboBox<Object> valutaComboBox = new ComboBox<>();
         placeComboBoxOnPane(valutaComboBox, layoutY);
         return valutaComboBox;
     }
-    private void placeComboBoxOnPane(ComboBox<Object> comboBox , int layoutY){
+
+    private void placeComboBoxOnPane(ComboBox<Object> comboBox, int layoutY) {
         comboBox.setLayoutX(STANDARD_INDENT + TEXTFIELD_WIDTH);
         comboBox.setLayoutY(layoutY);
         comboBox.setValue(CHOOSE_A_VALUTA);
     }
-    private ComboBox<Object> fillComboBox(ComboBox<Object> comboBox){
+
+    private ComboBox<Object> fillComboBox(ComboBox<Object> comboBox) {
         for (Currency currency : Currencies) {
             comboBox.getItems().add(currency.getName());
         }
@@ -136,11 +146,13 @@ public class CurrencyConverter extends Application {
     private TextField createInputField() {
         return createDefaultTextField(STANDARD_HEIGHT);
     }
+
     private TextField createOutputField() {
         TextField outputTextField = createDefaultTextField(STANDARD_HEIGHT + 30);
         outputTextField.setEditable(false);
         return outputTextField;
     }
+
     private TextField createDefaultTextField(int layoutY) {
         TextField defaultTextField = new TextField();
         defaultTextField.setLayoutX(STANDARD_INDENT);
